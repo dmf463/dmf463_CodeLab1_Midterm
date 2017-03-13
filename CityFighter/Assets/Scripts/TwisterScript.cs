6b1d443d640732e13c96e13e3a1a12eb40675f80
DavidFrisch <dmf463@nyu.edu>
2017-03-13 19:45:08 -0400

    got all the abilities done.

---
+++ CityFighter/Assets/Scripts/TwisterScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TwisterScript : MonoBehaviour {
+
+    public float timer;
+    public float returnTrigger;
+    bool isComingBack = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        timer += Time.deltaTime;
+        if (timer > returnTrigger && isComingBack == false)
+        {
+            isComingBack = true;
+            Debug.Log("COME BACK TO MEEE!!");
+            GetComponent<Rigidbody>().velocity *= -1;
+        }
+
+
+		
+	}
+}
